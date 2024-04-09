338349423cbf0e9089d726128a283a53f6710ceb
peetzweg/ <phil.czek@gmail.com>
2024-04-09 17:49:03 +0200

    adds open graph metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,8 +49,40 @@ export default IndexPage;
 export const Head: HeadFC = () => (
   <>
     <title>JAM</title>
-    <meta name="description" content="JAM Graypaper" />
+
     <link rel="stylesheet" href="https://latex.now.sh/style.css"></link>
+    <meta
+      name="description"
+      content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
+    />
+
+    {/* <!-- Facebook Meta Tags --> */}
+    <meta property="og:url" content="https://polkadot.network" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="JAM Graypaper" />
+    <meta
+      property="og:description"
+      content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
+    />
+    <meta
+      property="og:image"
+      content="https://opengraph.b-cdn.net/production/documents/83d1980a-1bb3-4db3-b64f-f05851e9cee0.png?token=nM98IeTAMa5qe5b0KkcqMGUxotMv6sbTNHKogUPKiIQ&height=630&width=1200&expires=33248677595"
+    />
+
+    {/* <!-- Twitter Meta Tags --> */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="polkadot.network" />
+    <meta property="twitter:url" content="https://polkadot.network" />
+    <meta name="twitter:title" content="JAM Graypaper" />
+    <meta
+      name="twitter:description"
+      content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
+    />
+    <meta
+      name="twitter:image"
+      content="https://opengraph.b-cdn.net/production/documents/83d1980a-1bb3-4db3-b64f-f05851e9cee0.png?token=nM98IeTAMa5qe5b0KkcqMGUxotMv6sbTNHKogUPKiIQ&height=630&width=1200&expires=33248677595"
+    />
+
     <body
       className="latex-dark bg-cover bg-black"
       style={{
