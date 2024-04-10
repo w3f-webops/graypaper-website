5ac89acc3dd5ee1b68c7bfdfba90e83502b4dda5
peetzweg/ <phil.czek@gmail.com>
2024-04-10 17:50:12 +0200

    redirect on 404 to main index page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,17 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
+import { PageProps } from "gatsby";
+import * as React from "react";
+import { createHead } from "../components/Head";
 const NotFoundPage: React.FC<PageProps> = () => {
+  React.useEffect(() => {
+    window.location.replace("/");
+  }, []);
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="h-screen w-screen flex flex-col justify-center items-center">
+      404
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head = createHead({ title: "404: Not found" });
