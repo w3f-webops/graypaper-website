94c5ee3257656897a0666d9e6d848199120ca070
peetzweg/ <phil.czek@gmail.com>
2024-04-12 16:01:46 +0200

    adds cn and jp abstract

--- src/i18n.ts
+++ src/i18n.ts
@@ -69,6 +69,11 @@ i18n
           "Coming soon": "近日公開...",
           "Resources.Download": "こちらでGraypaperのコピーを入手してください：",
           "Resources.Contribute": "[matrix]スペースでJAMに参加しよう：",
+          "Graypaper.Title":
+            '<span style="white-space: nowrap">JOIN-ACCUMULATE MACHINE:</span><br />セミコヒーレント スケーラブル トラストレス VM',
+          "Abstract.p1": `${KeywordAbstract} 我々は、<i>Polkadot</i>と<i>Ethereum</i>の両方の要素を組み合わせたプロトコルである${KeywordJam}の包括的かつ正式な定義を提示します。一つの矛盾のないモデルとして、${KeywordJam}はグローバルなシングルトンの許可不要のオブジェクト環境を提供します。これはEthereumによって先駆けられたスマートコントラクト環境と非常に似ており、スケーラブルなノードネットワーク上で並列化された安全なサイドバンド計算と組み合わされています。これはPolkadotによって先駆けられた提案です。`,
+          "Abstract.p2": `${KeywordJam}は、セキュアでスケーラブルなコア/オンチェーンの二元論を中心に構築されたスマートコントラクト機能を提供する分散型ハイブリッドシステムを導入します。スマートコントラクト機能はEthereumのパラダイムといくつかの類似点を示唆していますが、提供されるサービスの全体的なモデルは主にPolkadotの基盤となるアーキテクチャによって推進されています。`,
+          "Abstract.p3": `${KeywordJam}は許可不要の性質を持ち、誰でも使用するコードをサービスとして展開することができ、このコードが利用するリソースに見合った料金を支払い、<i>コアタイム</i>の調達と割り当てを通じてこのコードの実行を誘発することができます。これは、Ethereumでのガスの購入にやや似た、レジリエントで普遍的な計算の指標です。私たちはすでにPolkadot互換の<i>CoreChains</i>サービスを想定しています。`,
           "CTA.Download": "Graypaperを読む",
         },
       },
@@ -83,6 +88,11 @@ i18n
           "Coming soon": "即将推出...",
           "Resources.Download": "在这里获取Graypaper的副本：",
           "Resources.Contribute": "在我们的[matrix]空间加入JAM：",
+          "Graypaper.Title":
+            '<span style="white-space: nowrap">JOIN-ACCUMULATE MACHINE:</span><br />半一致可扩展无需信任的虚拟机',
+          "Abstract.p1": `${KeywordAbstract} 我们提出了一个全面和正式的定义${KeywordJam}，一个结合了<i>Polkadot</i>和<i>Ethereum</i>的元素的协议。在一个单一的连贯模型中，${KeywordJam}提供了一个全球单例无需许可的对象环境——非常类似于Ethereum开创的智能合约环境——与一个在可扩展节点网络上并行的安全边带计算相结合，这是Polkadot首创的提议。`,
+          "Abstract.p2": `${KeywordJam}引入了一个分散的混合系统，提供围绕安全和可扩展的核心/链上二元性结构的智能合约功能。虽然智能合约功能意味着与Ethereum的范式有一些相似之处，但所提供服务的整体模型主要是由Polkadot的底层架构驱动的。`,
+          "Abstract.p3": `${KeywordJam}的本质是无需许可的，允许任何人在其上部署代码作为服务，并根据该代码使用的资源支付相应的费用，并通过采购和分配<i>核心时间</i>来引导该代码的执行，这是一种类似于在Ethereum中购买气体的弹性和普遍的计算度量。我们已经设想了一个与Polkadot兼容的<i>CoreChains</i>服务。`,
           "CTA.Download": "阅读Graypaper",
         },
       },
