04144881f6fbbb41aa9c28d225d38d0ce620f7b6
peetzweg/ <phil.czek@gmail.com>
2024-04-12 11:22:58 +0200

    hardcode header trying to resolve hydration problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { createHead } from "../components/Head";
 import { Layout } from "../components/Layout";
 import Graypaper from "../images/graypaper.png";
+import { OpenGraph } from "../components/OpenGraph";
 
 const IndexPage: React.FC<PageProps> = (props) => {
   return (
@@ -78,6 +79,21 @@ const IndexPage: React.FC<PageProps> = (props) => {
 
 export default IndexPage;
 
-export const Head = createHead({
-  title: "JAM",
-});
+export const Head = (
+  <>
+    <html lang="en" />
+    <title>JAM</title>
+
+    <meta
+      name="description"
+      content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
+    />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+
+    <OpenGraph />
+  </>
+);
