83070aecd45023057e20c430da7ce0219f2366f7
peetzweg/ <phil.czek@gmail.com>
2024-04-12 15:12:34 +0200

    clean up

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,14 +1,11 @@
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
+
 export const LanguageSelect = () => {
-  const { t, i18n } = useTranslation();
-  console.log({ t, i18n });
-  i18n.languages;
+  const { i18n } = useTranslation();
   const changeLanguage = useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>) => {
-      console.log({ value: event.target.value });
       i18n.changeLanguage(event.target.value);
-      // navigate(`/${event.target.value.toLowerCase()}${window.location.pathname}`);
     },
     []
   );
