e17eb275f2928fd73d37bf0fdc5128b2268710a2
peetzweg/ <phil.czek@gmail.com>
2024-04-15 12:48:18 +0200

    upgrade packages

--- src/components/Head/CommonHead.tsx
+++ src/components/Head/CommonHead.tsx
@@ -1,18 +1,11 @@
-import React from "react";
-import { Favicon } from "./Favicon";
-import { OpenGraph } from "./OpenGraph";
+import React from "react"
+import { Favicon } from "./Favicon"
+import { OpenGraph } from "./OpenGraph"
 
 export const CommonHead: React.FC = () => (
   <>
-    <html lang="en" />
-
-    <meta
-      name="description"
-      content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
-    />
-
     <OpenGraph />
 
     <Favicon />
   </>
-);
+)
