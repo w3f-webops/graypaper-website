c3a9889a892799e8d885181ab4c1600aa0135c7f
kaichaosun <kaichaosuna@gmail.com>
2024-04-16 14:28:05 +0800

    more fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -89,7 +89,7 @@ i18n
           "Resources.Contribute": `加入我们的 ${KeywordJam} [matrix] 空间：`,
           "Graypaper.Title":
             '<span style="white-space: nowrap">JOIN-ACCUMULATE MACHINE:</span><br />准一致、可扩展、无需信任的虚拟机',
-          "Abstract.p1": `${KeywordAbstract} 我们提出了一个全面和正式的关于${KeywordJam}的定义，一个结合了<i>Polkadot</i>和<i>Ethereum</i>元素的协议。在一个单一的连贯模型中，${KeywordJam}提供了一个全局单例的、无需许可的对象环境——类似于Ethereum开创的智能合约环境，配合上安全的旁路计算，此计算可以并行地运行在可扩展的节点网络上，这是Polkadot带头提出的方案。`,
+          "Abstract.p1": `${KeywordAbstract} 我们提出了一个全面和正式的关于${KeywordJam}的定义，一个结合了<i>Polkadot</i>和<i>Ethereum</i>元素的协议。在一个单一的连贯模型中，${KeywordJam}提供了一个全局单例的、无需许可的对象环境——类似于Ethereum开创的智能合约环境，配合上安全的旁路计算，此计算可以并行地运行在可扩展的节点网络上，这是Polkadot首创的方案。`,
           "Abstract.p2": `${KeywordJam}引入了一个去中心的混合系统，提供围绕安全性和可扩展的in-core/on-chain二元性构建的智能合约功能。虽然智能合约功能意味着与Ethereum的范式有一些相似之处，但所提供的服务的整体模型主要是由Polkadot的底层架构驱动的。`,
           "Abstract.p3": `${KeywordJam}的本质是无需许可的，允许任何人在其上部署代码作为服务，并根据该代码使用的资源支付相应的费用，并通过购买和分配<i>core-time</i>来引导该代码的执行，core-time是一种弹性的、普遍的计算度量，类似于Ethereum中的gas费用。我们已经构想了一个与Polkadot兼容的<i>CoreChains</i>服务。`,
           "CTA.Download": "阅读Graypaper",
