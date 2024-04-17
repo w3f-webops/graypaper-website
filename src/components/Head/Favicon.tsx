56fcccac91d7b20d6e968c1866d6255ffe937dca
pczek <839848+peetzweg@users.noreply.github.com>
2024-04-17 10:49:18 +0200

    switch favicon depending on systems preferred color scheme

--- src/components/Head/Favicon.tsx
+++ src/components/Head/Favicon.tsx
@@ -2,9 +2,14 @@ import React from "react"
 
 export const Favicon: React.FC = () => (
   <>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link media="(prefers-color-scheme: dark)" rel="apple-touch-icon" sizes="180x180" href="/dark/apple-touch-icon.png" />
+    <link media="(prefers-color-scheme: dark)" rel="icon" type="image/png" sizes="32x32" href="/dark/favicon-32x32.png" />
+    <link media="(prefers-color-scheme: dark)" rel="icon" type="image/png" sizes="16x16" href="/dark/favicon-16x16.png" />
+    <link media="(prefers-color-scheme: dark)" rel="manifest" href="/dark/site.webmanifest" />
+
+    <link media="(prefers-color-scheme: light)" rel="apple-touch-icon" sizes="180x180" href="/light/apple-touch-icon.png" />
+    <link media="(prefers-color-scheme: light)" rel="icon" type="image/png" sizes="32x32" href="/light/favicon-32x32.png" />
+    <link media="(prefers-color-scheme: light)" rel="icon" type="image/png" sizes="16x16" href="/light/favicon-16x16.png" />
+    <link media="(prefers-color-scheme: light)" rel="manifest" href="/light/site.webmanifest" />
   </>
 )
