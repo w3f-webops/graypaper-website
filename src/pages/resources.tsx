ba18ce296692062d47a4c94d22356a216e7d70cc
peetzweg/ <phil.czek@gmail.com>
2024-05-16 16:40:23 +0200

    adds interview to resources page

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -10,48 +10,29 @@ const Page: React.FC<PageProps> = (props) => {
     <Layout>
       <article>
         <h2 id="Resources">{t("Resources")}</h2>
-        <table className="borders-custom">
-          <tbody>
-            <tr>
-              <td>
-                <a
-                  href="https://www.youtube.com/watch?v=xTMiE0UcZUo"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  TOKEN2049&#8212;Polkadot's Future: The Big JAM Presentation
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <a
-                  href="https://wiki.polkadot.network/docs/learn-jam-chain"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Polkadot Wiki&#8212;Polkadot's JAM Chain
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <a
-                  href="https://polkadot.polkassembly.io/referenda/682"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  OpenGov&#8212;Proposal #682 for JAM Ratification, Conformance,
-                  Performance by Polkadot Fellows
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
 
-        <h3 id="download">{t("Download")}</h3>
+        <nav>
+          <ol className=" space-y-3 pl-5 md:pl-10">
+            <li>
+              <a href="#download">Download</a>
+            </li>
+            <li>
+              <a href="#contribute">Contribute</a>
+            </li>
+            <li>
+              <a href="#interview">Interview</a>
+            </li>
+            <li>
+              <a href="#additional-resources">Additional Resources</a>
+            </li>
+          </ol>
+        </nav>
+
+        <h3 className="text-xl" id="download">
+          1. {t("Download")}
+        </h3>
         <p>{t("Resources.Download")}</p>
-        <p className="flex flex-col gap-4 pl-5 sm:pl-10">
+        <p className="flex flex-col gap-3 pl-5 md:pl-10">
           <a href="/graypaper.pdf" download={true}>
             <code>https://graypaper.com/graypaper.pdf</code>
           </a>
@@ -71,9 +52,12 @@ const Page: React.FC<PageProps> = (props) => {
             <code>https://github.com/gavofyork/graypaper</code>
           </a>
         </p>
-        <h3 id="contribute">{t("Contribute")}</h3>
+
+        <h3 className="text-xl" id="contribute">
+          2. {t("Contribute")}
+        </h3>
         <p dangerouslySetInnerHTML={{ __html: t("Resources.Contribute") }} />
-        <div className="flex flex-col items-start gap-4 pl-5 sm:pl-10">
+        <div className="flex flex-col items-start gap-3 pl-5 sm:pl-10">
           <a
             href="https://matrix.to/#/#jam:polkadot.io"
             target="_blank"
@@ -89,6 +73,54 @@ const Page: React.FC<PageProps> = (props) => {
             <code>#graypaper:polkadot.io</code>
           </a>
         </div>
+
+        <h3 className="text-xl" id="interview">
+          3. {t("Interview")}
+        </h3>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/O3kRAVBTkfs?si=auiYGd6pQfrYV2D4"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+
+        <h3 className="text-xl" id="additional-resources">
+          4. {t("Additional Resources")}
+        </h3>
+        <ul className="list-disc  space-y-3 pl-5 md:pl-10">
+          <li>
+            <a
+              href="https://www.youtube.com/watch?v=xTMiE0UcZUo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              TOKEN2049&#8212;Polkadot's Future: The Big JAM Presentation
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://wiki.polkadot.network/docs/learn-jam-chain"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Polkadot Wiki&#8212;Polkadot's JAM Chain
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://polkadot.polkassembly.io/referenda/682"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              OpenGov&#8212;Proposal #682 for JAM Ratification, Conformance,
+              Performance by Polkadot Fellows
+            </a>
+          </li>
+        </ul>
       </article>
     </Layout>
   )
