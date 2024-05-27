6c372e6345dcb1698f310dd72a0bca5c1b9c3011
peetzweg/ <phil.czek@gmail.com>
2024-05-27 13:19:32 +0200

    adds news about auction end and confirmation of #682

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -4,8 +4,63 @@ import { useTranslation } from "react-i18next"
 import { CommonHead } from "../components/Head/CommonHead"
 import { Layout } from "../components/Layout"
 
+const newsData = [
+  {
+    date: "27/05/2024",
+    title: "OpenGov—Proposal #682 for JAM Ratification Confirmed",
+    link: "https://polkadot.polkassembly.io/referenda/682",
+  },
+  {
+    date: "23/05/2024",
+    title: "Charity Auction—Annotated JAM Graypaper sold for over $17,000",
+    link: "https://gavinwood.gbm.auction",
+  },
+  {
+    date: "09/05/2024",
+    title: "X Space—Gavin Wood on JAM: The next disruptor in Web3",
+    link: "https://twitter.com/Polkadot/status/1788614918796607848",
+  },
+  {
+    date: "06/05/2024",
+    title: "Charity Bid-to-Earn Auction—May 16th, 2024",
+    link: "https://gavinwood.gbm.auction",
+  },
+  {
+    date: "29/04/2024",
+    title: "TOKEN2049—Polkadot's Future: The Big JAM Presentation",
+    link: "https://www.youtube.com/watch?v=xTMiE0UcZUo",
+  },
+  {
+    date: "28/04/2024",
+    title: "Gavin Wood: The Gray Paper Interview—JAM & the Future of Polkadot",
+    link: "https://www.youtube.com/watch?v=O3kRAVBTkfs",
+  },
+  {
+    date: "18/04/2024",
+    title: "Polkadot Wiki—Polkadot's JAM Chain",
+    link: "https://wiki.polkadot.network/docs/learn-jam-chain",
+  },
+  {
+    date: "18/04/2024",
+    title:
+      "OpenGov—Proposal #682 for JAM Ratification, Conformance, Performance by Polkadot Fellows",
+    link: "https://polkadot.polkassembly.io/referenda/682",
+  },
+  {
+    date: "18/04/2024",
+    title: "TOKEN2049—The Big JAM Slides Released",
+    link: "/The_Big_JAM.pdf",
+  },
+  {
+    date: "18/04/2024",
+    title: "Announcing the JAM Implementer’s Prize",
+    link: "https://medium.com/web3foundation/announcing-the-jam-implementers-prize-e79f8ebc506d",
+  },
+]
+
 const Page: React.FC<PageProps> = (props) => {
   const { t } = useTranslation()
+
   return (
     <Layout>
       <article>
@@ -13,104 +68,20 @@ const Page: React.FC<PageProps> = (props) => {
 
         <table className="borders-custom">
           <tbody>
-            <tr>
-              <td>09/05/2024</td>
-              <td>
-                <a
-                  href="https://twitter.com/Polkadot/status/1788614918796607848"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  X Space&#8212;Gavin Wood on JAM: The next disruptor in Web3
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>06/05/2024</td>
-              <td>
-                <a
-                  href="https://gavinwood.gbm.auction"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Charity Bid-to-Earn Auction&#8212;May 16th, 2024
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>29/04/2024</td>
-              <td>
-                <a
-                  href="https://www.youtube.com/watch?v=xTMiE0UcZUo"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  TOKEN2049&#8212;Polkadot's Future: The Big JAM Presentation
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>28/04/2024</td>
-              <td>
-                <a
-                  href="https://www.youtube.com/watch?v=O3kRAVBTkfs"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Gavin Wood: The Gray Paper Interview&#8212;JAM & the Future of
-                  Polkadot
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>18/04/2024</td>
-              <td>
-                <a
-                  href="https://wiki.polkadot.network/docs/learn-jam-chain"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Polkadot Wiki&#8212;Polkadot's JAM Chain
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>18/04/2024</td>
-              <td>
-                <a
-                  href="https://polkadot.polkassembly.io/referenda/682"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  OpenGov&#8212;Proposal #682 for JAM Ratification, Conformance,
-                  Performance by Polkadot Fellows
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>18/04/2024</td>
-              <td>
-                <a
-                  href="/The_Big_JAM.pdf"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  TOKEN2049&#8212;The Big JAM Slides Released
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>18/04/2024</td>
-              <td>
-                <a
-                  href="https://medium.com/web3foundation/announcing-the-jam-implementers-prize-e79f8ebc506d"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Announcing the JAM Implementer’s Prize
-                </a>
-              </td>
-            </tr>
+            {newsData.map((newsItem, index) => (
+              <tr key={index}>
+                <td>{newsItem.date}</td>
+                <td>
+                  <a
+                    href={newsItem.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {newsItem.title}
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </article>
