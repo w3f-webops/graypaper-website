1fe1fc67893017d9d7be468342b08118f33f6f26
peetzweg/ <phil.czek@gmail.com>
2024-07-23 12:24:31 +0200

    lists how many clients are listed and in which language set

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -5,23 +5,32 @@ import { CommonHead } from "../components/Head/CommonHead"
 import { Layout } from "../components/Layout"
 import clients from "../data/clients.json"
 
-type Client = typeof clients[number];
+type Client = (typeof clients)[number]
 const sortClients = (a: Client, b: Client) => {
   // First, compare by lang_set
-  const langSetComparison = a.lang_set.localeCompare(b.lang_set);
-  if (langSetComparison !== 0) return langSetComparison;
+  const langSetComparison = a.lang_set.localeCompare(b.lang_set)
+  if (langSetComparison !== 0) return langSetComparison
 
   // If lang_set is the same, compare by lang
-  const langComparison = a.lang.localeCompare(b.lang);
-  if (langComparison !== 0) return langComparison;
+  const langComparison = a.lang.localeCompare(b.lang)
+  if (langComparison !== 0) return langComparison
 
   // If lang is also the same, compare by milestone (descending order)
-  const milestoneComparison = b.milestone - a.milestone;
-  if (milestoneComparison !== 0) return milestoneComparison;
+  const milestoneComparison = b.milestone - a.milestone
+  if (milestoneComparison !== 0) return milestoneComparison
 
   // If milestone is also the same, compare by name
-  return a.name.localeCompare(b.name);
-};
+  return a.name.localeCompare(b.name)
+}
+
+const ClientsPerLanguageSet = clients.reduce(
+  (prev: Record<string, number>, cur) => {
+    prev[cur.lang_set] = prev[cur.lang_set] || 0
+    prev[cur.lang_set] += 1
+    return prev
+  },
+  {},
+)
 
 const Page: React.FC<PageProps> = (props) => {
   const { t } = useTranslation()
@@ -29,15 +38,37 @@ const Page: React.FC<PageProps> = (props) => {
   return (
     <Layout>
       <article>
-        <h2 id="Clients" className="text-center">{t("Clients")}</h2>
+        <h2 id="Clients" className="text-center">
+          {t("Clients")}
+        </h2>
 
         <p>
-          The <a href="/prize"><span className="jam">
-            <span>J</span>am
-          </span> {" "}Implementer’s Prize</a> encourages multiple
-          client implementations to boost network resilience. The following
-          table list the publicly known clients in development.
+          The{" "}
+          <a href="/prize">
+            <span className="jam">
+              <span>J</span>am
+            </span>{" "}
+            Implementer’s Prize
+          </a>{" "}
+          encourages multiple client implementations to boost network
+          resilience. The table on this page lists the publicly known clients in
+          development.
         </p>
+        <p>
+          As of now there are {clients.length} clients listed across the{" "}
+          <a href="https://jam.web3.foundation/#languages" target="_blank">
+            5 language sets
+          </a>
+          :
+        </p>
+
+        <p className="text-center">
+          <em>A</em>: {ClientsPerLanguageSet["A"]}, <em>B</em>:{" "}
+          {ClientsPerLanguageSet["B"]}, <em>C</em>: {ClientsPerLanguageSet["C"]}
+          , <em>D</em>: {ClientsPerLanguageSet["D"]}, <em>Z</em>:{" "}
+          {ClientsPerLanguageSet["Z"] || 0}
+        </p>
+
         <p>
           If you want to list your client or update your listing, please request
           the change{" "}
@@ -51,7 +82,6 @@ const Page: React.FC<PageProps> = (props) => {
         </p>
         <div className="overflow-x-scroll py-4 font-normal">
           <table className="col-1-l col-2-c col-3-l col-4-c col-5-c col-6-l">
-
             <thead>
               <tr>
                 <th>Name</th>
@@ -63,37 +93,48 @@ const Page: React.FC<PageProps> = (props) => {
               </tr>
             </thead>
             <tbody>
-              {clients
-                .sort(sortClients)
-                .map((client, index) => (
-                  <tr key={index} className="borders-custom">
-                    <td className="text-nowrap">{client.name}</td>
-                    <td><em>{client.lang_set}</em></td>
-                    <td>{client.lang}</td>
-                    <td>{client.milestone > 0 ? client.milestone : "-"}</td>
-                    <td>
-                      {client.homepage.length === 0 ? "-" :
-                        <a target="_blank" href={client.homepage}>{`[${index + 1}]`}</a>}
-                    </td>
-                    <td className="w-full text-nowrap font-mono text-sm">{client.contact.join(", ")}</td>
-                  </tr>
-                ))}
+              {clients.sort(sortClients).map((client, index) => (
+                <tr key={index} className="borders-custom">
+                  <td className="text-nowrap">{client.name}</td>
+                  <td>
+                    <em>{client.lang_set}</em>
+                  </td>
+                  <td>{client.lang}</td>
+                  <td>{client.milestone > 0 ? client.milestone : "-"}</td>
+                  <td>
+                    {client.homepage.length === 0 ? (
+                      "-"
+                    ) : (
+                      <a
+                        target="_blank"
+                        href={client.homepage}
+                      >{`[${index + 1}]`}</a>
+                    )}
+                  </td>
+                  <td className="w-full text-nowrap font-mono text-sm">
+                    {client.contact.join(", ")}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
 
-        <table className="borders-custom text-sm col-1-r font-normal leading-3">
+        <table className="borders-custom col-1-r text-sm font-normal leading-3">
           {clients.map((client, index) => {
             if (client.homepage.length === 0) return null
             return (
               <tr key={index} className="borders-custom">
                 <td>{`[${index + 1}]`}</td>
-                <td><a target="_blank" href={client.homepage}>{client.homepage}</a></td>
+                <td>
+                  <a target="_blank" href={client.homepage}>
+                    {client.homepage}
+                  </a>
+                </td>
               </tr>
             )
           })}
         </table>
-
       </article>
     </Layout>
   )
