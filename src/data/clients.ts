3a0090713a925cd3ffcb8843c764940c6d1c596a
Daniel Cukier <danicuki@gmail.com>
2024-08-23 10:19:00 +0100

    change jamixir to official domain (#32)

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -228,12 +228,12 @@ export const clients: ClientData[] = [
   },
   {
     description: "PBA Alumni Implementing JAM in Elixir.",
-    homepage: "https://t.me/+jlaRqhcS06o1YTYx",
+    homepage: "http://jamixir.org/",
     name: "Jamixir",
     lang: "Elixir",
     lang_set: "D",
     milestone: 0,
-    contact: ["danicuki+jamixir@gmail.com", "daiagi@gmail.com"],
+    contact: ["daniel@jamixir.org", "daiagi@jamixir.org"],
   },
   {
     description:
