d5b8e341b3ecc8bc2cad5e4a1efdd8ea951c4769
peetzweg/ <phil.czek@gmail.com>
2024-09-10 11:17:24 +0200

    adds all missing keys to all language files

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -5,52 +5,52 @@ export default {
     "Abstract.p2": `${KeywordJam} introduces a decentralized hybrid system offering smart-contract functionality structured around a secure and scalable in-core/on-chain dualism. While the smart-contract functionality implies some similarities with Ethereum&#x2019;s paradigm, the overall model of the service offered is driven largely by underlying architecture of Polkadot.`,
     "Abstract.p3": `${KeywordJam} is permissionless in nature, allowing anyone to deploy code as a service on it for a fee commensurate with the resources this code utilizes and to induce execution of this code through the procurement and allocation of <i>core-time</i>, a metric of resilient and ubiquitous computation, somewhat similar to the purchasing of gas in Ethereum. We already envision a Polkadot-compatible <i>CoreChains</i> service.`,
     "Additional Resources": "Additional Resources",
-    "Coming soon": "Coming soon...",
     "Clients": "Clients",
     "Clients.why": `
-        The <a href="/prize">${KeywordJam} Implementer’s Prize</a>
-        encourages multiple client implementations to boost network
-        resilience.
-        The table on this page lists the publicly known clients in development.
+    The <a href="/prize">${KeywordJam} Implementer’s Prize</a>
+    encourages multiple client implementations to boost network
+    resilience.
+    The table on this page lists the publicly known clients in development.
     `,
     "Clients.amount": `
-        As of now there are {{numberOfClients}} clients listed across the <a href="https://jam.web3.foundation/#languages" target="_blank"> 5 language sets</a>:`,
+    As of now there are {{numberOfClients}} clients listed across the <a href="https://jam.web3.foundation/#languages" target="_blank"> 5 language sets</a>:`,
     "Clients.add": `
-        If you want to list your client or update your listing, please request the change <a target="_blank" href="https://github.com/w3f-webops/graypaper-website/issues/new?assignees=&labels=&projects=&template=client-listing-request.md&title=Client+Listing%3A+XYZ"> here</a>.`,
+    If you want to list your client or update your listing, please request the change <a target="_blank" href="https://github.com/w3f-webops/graypaper-website/issues/new?assignees=&labels=&projects=&template=client-listing-request.md&title=Client+Listing%3A+XYZ"> here</a>.`,
     "Clients.name": "Name",
     "Clients.set": "Set",
     "Clients.language": "Language",
     "Clients.milestone": "Milestone",
     "Clients.web": "Web",
     "Clients.contact": "Contact",
+    "Coming soon": "Coming soon...",
+    "Contribute": "Contribute",
     "CTA.Download": "Read the Gray Paper",
+    "Download": "Download",
     "filmed.brussels": "Filmed at the The Sneakers Cafe in Brussels, Belgium on July 11, 2024",
     "filmed.buenos.aires": "Filmed in Buenos Aires on May 10, 2024",
     "filmed.singapore": "Filmed at the National University of Singapore on June 19, 2024.",
     "GrayPaper.Title": '<span style="white-space: nowrap">JOIN-ACCUMULATE MACHINE:</span><br />A SEMI-COHERENT SCALABLE TRUSTLESS VM',
     "Hide Gray Paper": "Hide Gray Paper",
+    "Interview": "Interview",
     "Latest version": "Latest version",
+    "Lectures": "Lectures",
+    "News": "News",
     "Next Section": "Next Section",
     "Previous Section": "Previous Section",
     "Prize.apply": `The ${KeywordJam} Implementer’s Prize encourages multiple client implementations to boost network resilience. The Foundation seeks proposals for ${KeywordJam} Implementer’s Prize here:`,
     "Prize.cta": "Read the announcement",
     "Prize.description": `The Web3 Foundation announces the ${KeywordJam} Implementer&#x2019;s Prize, a 10 Million DOT prize pool aimed at fostering diversity in the development of the ${KeywordJam} protocol. The goal of this prize is to encourage the creation of multiple client implementations, enhancing the network&#x2019;s resilience.`,
+    "Prize": "Prize",
     "Resources.Contribute": `Join the ${KeywordJam} in our ${KeywordMatrix} rooms:`,
     "Resources.DecisiveVersion": "on GitHub is the decisive version",
     "Resources.Download": "Get a copy of the Gray Paper here:",
     "Resources.Latex": "or compile the LaTeX from source:",
     "Resources.MostRecent": "most recent official released version",
     "Resources.VersionNoBackground": "A version without the dark background is provided for your convenience. The",
+    "Resources": "Resources",
     "Select Lecture": "Select a Lecture",
     "Show Section in Gray Paper": "Show Section in Gray Paper",
     "Tour Description": "Gavin Wood will be appearing at universities around the world in a series of lectures to take viewers through the Gray Paper and explain each part in depth.",
     "Tour YouTube": `Recordings of this tour are shared on the <a href="https://www.youtube.com/@JamPrizeTour" target="_blank">  ${KeywordJam} Prize Tour YouTube Channel</a>. See the <a href="/lectures">Lectures</a> section for studying the Gray Paper along with the Tour material.`,
-    Contribute: "Contribute",
-    Download: "Download",
-    Interview: "Interview",
-    Lectures: "Lectures",
-    News: "News",
-    Prize: "Prize",
-    Resources: "Resources",
-    Tour: "Tour",
+    "Tour": "Tour",
 }
