214e13e7c203e2ac7dced4024843f27bfc60b428
YC_Chen <mo0307b1006@gmail.com>
2024-11-09 09:43:06 +0800

    Update clients.ts

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -2,6 +2,15 @@ import { ClientData } from "./types"
 
 export const clients: ClientData[] = [
   {
+    description: "The first JAM team in Taiwan. Make JAM great again.",
+    homepage: "https://hackmd.io/8ckvpUULSp-HqThsxXE3jg?view",
+    name: "New JAMreation",
+    lang: "Go",
+    lang_set: "A",
+    milestone: 0,
+    contact: ["mo0307b1@gmail.com"],
+  },
+  {
     description: "Parity experimental JAM client. Currently closed source.",
     homepage: "",
     name: "PolkaJam",
