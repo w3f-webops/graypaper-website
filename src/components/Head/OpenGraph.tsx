563717102f9bf7de7bf456c89b1a2c031ea8b966
pczek <839848+peetzweg@users.noreply.github.com>
2024-12-12 07:43:48 +0700

    use fully qualified link for default og image

--- src/components/Head/OpenGraph.tsx
+++ src/components/Head/OpenGraph.tsx
@@ -9,7 +9,10 @@ export const OpenGraph: React.FC = () => (
       property="og:description"
       content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
     />
-    <meta property="og:image" content="/img/opengraph.png" />
+    <meta
+      property="og:image"
+      content="https://graypaper.com/img/opengraph.png"
+    />
 
     {/* <!-- Twitter Meta Tags --> */}
     <meta id="twitter:card" name="twitter:card" content="summary_large_image" />
@@ -32,7 +35,13 @@ export const OpenGraph: React.FC = () => (
     <meta
       id="twitter:image"
       name="twitter:image"
-      content="/img/opengraph.png"
+      content="https://graypaper.com/img/opengraph.png"
     />
+    <meta
+      id="twitter:creator"
+      name="twitter:creator"
+      content="@Web3foundation"
+    />
+    <meta id="twitter:site" name="twitter:site" content="@Web3foundation" />
   </>
 )
