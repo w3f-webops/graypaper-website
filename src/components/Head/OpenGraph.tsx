858a7fd92fb67f96c37d0e16f93e87b7c4ba1b44
peetzweg/ <839848+peetzweg@users.noreply.github.com>
2024-12-12 07:47:04 +0700

    fix: twitter cards (#62)
    * unique meta tags for prize page for twitter card
    
    * use fully qualified link
    
    * use fully qualified link for default og image

--- src/components/Head/OpenGraph.tsx
+++ src/components/Head/OpenGraph.tsx
@@ -9,17 +9,39 @@ export const OpenGraph: React.FC = () => (
       property="og:description"
       content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
     />
-    <meta property="og:image" content="/img/opengraph.png" />
+    <meta
+      property="og:image"
+      content="https://graypaper.com/img/opengraph.png"
+    />
 
     {/* <!-- Twitter Meta Tags --> */}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta property="twitter:domain" content="graypaper.com" />
-    <meta property="twitter:url" content="https://graypaper.com" />
-    <meta name="twitter:title" content="JAM Gray Paper" />
+    <meta id="twitter:card" name="twitter:card" content="summary_large_image" />
     <meta
+      id="twitter:domain"
+      property="twitter:domain"
+      content="graypaper.com"
+    />
+    <meta
+      id="twitter:url"
+      property="twitter:url"
+      content="https://graypaper.com"
+    />
+    <meta id="twitter:title" name="twitter:title" content="JAM Gray Paper" />
+    <meta
+      id="twitter:description"
       name="twitter:description"
       content="JOIN-ACCUMULATE MACHINE: A SEMI-COHERENT SCALABLE TRUSTLESS VM"
     />
-    <meta name="twitter:image" content="/img/opengraph.png" />
+    <meta
+      id="twitter:image"
+      name="twitter:image"
+      content="https://graypaper.com/img/opengraph.png"
+    />
+    <meta
+      id="twitter:creator"
+      name="twitter:creator"
+      content="@Web3foundation"
+    />
+    <meta id="twitter:site" name="twitter:site" content="@Web3foundation" />
   </>
 )
