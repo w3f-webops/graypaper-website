71f00d00d56fcc7c751dc5d946d6e4e9f3f50bd6
peetzweg/ <839848+peetzweg@users.noreply.github.com>
2025-02-26 14:12:29 +0100

    news: pvm debugger demo (#84)
    pvm debugger demo

--- src/data/news.ts
+++ src/data/news.ts
@@ -2,6 +2,11 @@ import { News } from "./types"
 
 export const news: News[] = [
   {
+    title: "Polkadot Virtual Machine (PVM) simulation: Fibonacci Program",
+    date: "26/02/2025",
+    link: "https://www.youtube.com/watch?v=eWB0WQBXE5s",
+  },
+  {
     title: "The JAM experience. What’s it really like?",
     date: "13/02/2025",
     link: "https://www.parity.io/blog/JAM-experience",
