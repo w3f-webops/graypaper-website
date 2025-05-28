f0e36983c80e651a5c86b0833b10285812990390
pczek <839848+peetzweg@users.noreply.github.com>
2025-05-28 13:05:58 +0200

    adds jamixir interview

--- src/data/news.ts
+++ src/data/news.ts
@@ -2,6 +2,11 @@ import { News } from "./types"
 
 export const news: News[] = [
   {
+    link: "https://www.youtube.com/watch?v=gKrWeXKorEM",
+    date: "28/05/2025",
+    title: "Pala Labs—Jamixir: From CTO to JAM Builder | JAM Implementers #4",
+  },
+  {
     link: "https://www.youtube.com/watch?v=8gfZW454CeY",
     date: "25/05/2025",
     title: "Polkadot Future: JAM Protocol by Gavin Wood—Hong Kong Keynote",
